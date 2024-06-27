dd69bc88aa8a204fb9b243e7a2de974b8cfe17a3
Hyojun Ahn <sherlock216@korea.ac.kr>
2024-06-27 17:28:04 +0900

    complete0627

--- public/firebase.js
+++ public/firebase.js
@@ -1,20 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-analytics.js";
 import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-messaging.js";
+import { firebaseConfig, vapidKey } from '../config.js';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyCkH5uKfM1lARxtESEwWJV4uo9ThiYqg0I",
-    authDomain: "test-diary-e670f.firebaseapp.com",
-    projectId: "test-diary-e670f",
-    storageBucket: "test-diary-e670f.appspot.com",
-    messagingSenderId: "31104531425",
-    appId: "1:31104531425:web:9ec2169be7ea160a1f30a0",
-    measurementId: "G-63DVE4VR0L"
-};
+
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
@@ -24,7 +17,7 @@ navigator.serviceWorker.register('../firebase-messaging-sw.js')
     .then((registration) => {
         return getToken(messaging, {
             ServiceWorkerRegistration: registration,
-            vapidKey: 'BNR3LCM6k73nBWp474Ms3oz-0KozcwSJ99g_T9rWcPodRG8_q-aZCEK0PBBXPj2QO5otyuNDwVvEOrWT544QKoU'
+            vapidKey: vapidKey
         });
     })
     .then((currentToken) => {
